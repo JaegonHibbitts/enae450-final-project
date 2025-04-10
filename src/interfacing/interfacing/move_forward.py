24491cedaf05a43870e85ec29e9ca55744da0a0c
icf3ver <lballmer@terpmail.umd.edu>
2025-04-10 16:56:46 -0400

    commit 2

--- src/interfacing/interfacing/move_forward.py
+++ src/interfacing/interfacing/move_forward.py
@@ -4,31 +4,40 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
 class movement(Node):
     def __init__(self):
         super().__init__("number_publisher")
         #create node class
-       
+        self.i = 0
         self.forward_velocity_ = self.create_publisher(Twist, "cmd_vel", 10)
+        self.timer = self.create_timer(0.5, self.set_velocity)
+        
+        msg = Twist()
+        msg.linear.x = float(.0)
+
+        self.forward_velocity_.publish(msg)
+
         # create publisher under type Twist
        
-    def set_velocity(self,value):
+    def set_velocity(self):
         msg = Twist()
-        msg.linear.x = value
+        if self.i < 6:
+           msg.linear.x = float(.1)
+        else:
+           msg.linear.x = float(.0)
+
         # set linear velocity
         self.forward_velocity_.publish(msg)
         #publish message
-        self.get_logger().info('Publishing: "%d"' % msg.linear.x)
+        self.get_logger().info(f'Publishing: "{msg.linear.x}"')
+        self.i += 1
         #log publish
 
 def main(args=None):
     rclpy.init(args=args)
     node = movement()
-    # make node
-    node.set_velocity(.1)
-    # set velocity
-    # node.set_velocity(0)
     # make node
     rclpy.spin(node)
     rclpy.shutdown()
