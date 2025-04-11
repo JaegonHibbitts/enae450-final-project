9df0862c1d169ed179d71e0308f0264f3d7de170
icf3ver <luke.ballmer@outlook.com>
2025-04-11 17:18:48 -0400

    Lidar Experimentation

--- src/interfacing/interfacing/move_forward.py
+++ src/interfacing/interfacing/move_forward.py
@@ -6,32 +6,41 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+import numpy
+
 class movement(Node):
     def __init__(self):
         super().__init__("number_publisher")
         #create node class
-        self.i = 0
+       
+        self.Lidardis = self.create_subscription(LaserScan, "scan", self.obstacle, 10)
+       
         self.forward_velocity_ = self.create_publisher(Twist, "cmd_vel", 10)
         self.timer = self.create_timer(0.5, self.set_velocity)
-        
-        msg = Twist()
-        msg.linear.x = float(.0)
-
-        self.forward_velocity_.publish(msg)
-
         # create publisher under type Twist
+        self.i = 0
+        self.stop = 0
+       
+    def obstacle(self,msg):
+        x = len(msg.ranges)//3
+        for j in range(x):
+            i = j + x
+            if (msg.ranges[i] < .5) and (msg.ranges[i] > msg.range_min):
+                self.stop = 1
+                break
+            else:
+                self.stop = 0
        
     def set_velocity(self):
         msg = Twist()
-        if self.i < 6:
-           msg.linear.x = float(.1)
+        if (self.stop == 0):
+           msg.linear.x = .1
         else:
-           msg.linear.x = float(.0)
-
+           msg.linear.x = .0
         # set linear velocity
         self.forward_velocity_.publish(msg)
         #publish message
-        self.get_logger().info(f'Publishing: "{msg.linear.x}"')
+        self.get_logger().info('Publishing: "%d"' % msg.linear.x)
         self.i += 1
         #log publish
 
