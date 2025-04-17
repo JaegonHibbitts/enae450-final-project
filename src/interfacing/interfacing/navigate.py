64ec10175c9cae43c4094eb80e1bfc5bb011cc48
aschaef8-umd <aschaef8@umd.edu>
2025-04-17 16:09:23 -0400

    Add files via upload

---
+++ src/interfacing/interfacing/navigate.py
@@ -0,0 +1,96 @@
+from time import sleep
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+import numpy
+
+class movement(Node):
+    def __init__(self):
+        super().__init__("number_publisher")
+        #create node class
+       
+        self.Lidardis = self.create_subscription(LaserScan, "scan", self.obstacle, 10)
+       
+        self.forward_velocity_ = self.create_publisher(Twist, "cmd_vel", 10)
+        self.timer = self.create_timer(0.5, self.set_velocity)
+        # create publisher under type Twist
+        self.i = 0
+        self.stop = 0
+
+    def navigate(self,msg):
+        if self.stop == 1:
+            x = len(msg.ranges)//6
+            for j in range(x):
+                i = j + x
+                if (msg.ranges[i] > 1) and (msg.ranges[i] < msg.range_max):
+                    while(self.stop == 1):
+                        self.turnL
+                    self.turnL
+                    break
+            for j in range(x):
+                i = j + 4*x
+                if (msg.ranges[i] > 1) and (msg.ranges[i] < msg.range_max):
+                    while(self.stop == 1):
+                        self.turnR
+                    self.turnR
+                    break
+            while(self.stop == 1):
+                self.turnL
+            self.turnL
+
+
+       
+    def obstacle(self,msg):
+        x = len(msg.ranges)//3
+        for j in range(x):
+            i = j + x
+            if (msg.ranges[i] < .5) and (msg.ranges[i] > msg.range_min):
+                self.stop = 1
+                break
+            else:
+                self.stop = 0
+      
+                
+    def set_velocity(self):
+        msg = Twist()
+        if (self.stop == 0):
+           msg.linear.x = .1
+        else:
+           msg.linear.x = .0
+        # set linear velocity
+        self.forward_velocity_.publish(msg)
+        #publish message
+        self.get_logger().info('Publishing: "%d"' % msg.linear.x)
+        self.i += 1
+        #log publish
+
+    def turnL(self):
+        msg = Twist()
+        if self.stop == 1:
+            msg.angular.z = -.1
+        else:
+            msg.angular.z = 0
+
+    def turnR(self):
+        msg = Twist()
+        if self.stop == 1:
+            msg.angular.z = .1
+        else:
+            msg.angular.z = 0
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = movement()
+    # make node
+    rclpy.spin(node)
+    rclpy.shutdown()
+    # shutdown node
+
+
+if __name__ == '__main__':
+    main()
