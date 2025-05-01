5a65e69c842da05fb9bf08dfc0a2eebc9884e017
Jaegon Hibbitts <jhaegon123@gmail.com>
2025-05-01 15:55:27 -0400

    Patch 1

--- src/interfacing/interfacing/loop_around_wallfollow.py
+++ src/interfacing/interfacing/loop_around_wallfollow.py
@@ -30,37 +30,43 @@ class movement(Node):
         self.timer = self.create_timer(0.5, self.set_velocity) # rate of cmds
         
         # Calibration settings
-        self.act_dist = 0.3
+        self.act_dist = 0.38
 
         # Toggles
         self.stop = 0
         self.lpub = False
         self.rpub = False
+        self.slpub = False
+        self.srpub = False # Unused
+        self.turn_timer_slow = 0
+        self.turn_timer_slow_max = 2 # seconds
 
     def navigate(self,msg):
         self.get_logger().info('Turning')
-        x = len(msg.ranges) // 8
+        x = len(msg.ranges) // 10
 
         lwall = False
         rwall = False
         # Left
-        for j in range(int(x*1.1)): # NOTE: this sensor is slightly larger!
-            i = j + 2*x
-            if (msg.ranges[i] < self.act_dist) and (msg.ranges[i] > msg.range_min):
-                lwall = True
-
-        # Right
-        for j in range(x):
+        for j in range(int(2*x)): # NOTE: this sensor is slightly larger!
             i = j + 5*x
             if (msg.ranges[i] < self.act_dist) and (msg.ranges[i] > msg.range_min):
+                lwall = True
+                self.get_logger().info('Left wall detected')
+
+        # Right
+        for j in range(2*x):
+            i = j + 3*x
+            if (msg.ranges[i] < self.act_dist) and (msg.ranges[i] > msg.range_min):
                 rwall = True
+                self.get_logger().info('Right wall detected')
         
-        # Engage motor actions 
-        if self.stop == 1: # Stopped
-            if rwall: self.lpub = True
-            elif lwall: self.rpub = True
-        elif not rwall and not lwall: 
-            self.lpub = True
+        # Engage motor actions     
+        if not rwall and not lwall: 
+            self.slpub = True
+            self.get_logger().info('No detection')
+        elif rwall: self.lpub = True
+        elif lwall: self.rpub = True
         else:
             # Stop rotation here (No timer callback)
             msg = Twist()
@@ -70,6 +76,7 @@ class movement(Node):
 
     def obstacle(self,msg):
         x = len(msg.ranges)//8
+        # Front
         for j in range(2*x):
             i = j + 3*x
             if (msg.ranges[i] < self.act_dist) and (msg.ranges[i] > msg.range_min):
@@ -84,7 +91,7 @@ class movement(Node):
     def set_velocity(self):
         msg = Twist()
         if (self.stop == 0):
-           msg.linear.x = .1
+           msg.linear.x = .075
            self.get_logger().info('Moving')
         else:
            msg.linear.x = .0
@@ -92,26 +99,39 @@ class movement(Node):
 
         # set linear velocity
         self.velocity_.publish(msg)
-
+        
         # TODO test and calibrate
         if self.lpub:
             self.turnL()
             self.lpub = False
-            self.get_logger().info('Turning L')
+            self.get_logger().info('Quick Turning L')
         elif self.rpub:
             self.turnR()
             self.rpub = False
-            self.get_logger().info('Turning R')
+            self.get_logger().info('Quick Turning R')
+        
+        if self.turn_timer_slow % self.turn_timer_slow_max == 0:
+            if self.slpub:
+                self.turnL()
+                self.slpub = False
+                self.get_logger().info('Turning L')
+            elif self.srpub:
+                self.turnR()
+                self.srpub = False
+                self.get_logger().info('Turning R')
+            
+        self.turn_timer_slow += 0.5
+        self.turn_timer_slow %= self.turn_timer_slow_max
         
 
     def turnL(self):
         msg = Twist()
-        msg.angular.z = -.3 # Calibrate the arc
+        msg.angular.z = 0.25 # Calibrate the arc
         self.velocity_.publish(msg)
 
     def turnR(self):
         msg = Twist()
-        msg.angular.z = .3 # Good, speed of rotate away from opposing obstacle
+        msg.angular.z = -0.25 # Good, speed of rotate away from opposing obstacle
         self.velocity_.publish(msg)
 
 
@@ -127,3 +147,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
